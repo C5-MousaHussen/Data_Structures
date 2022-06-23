a502f2e7eb7d11dbf3fb14c0675abf54657ce7c2
MousaHussen <Mousahusseen@gmail.com>
2022-06-23 12:00:27 +0300

    solving queue

--- Round-0/1. queue/index.js
+++ Round-0/1. queue/index.js
@@ -14,6 +14,33 @@
     q.enqueue(5); // queue is now [1, 5]
     q.dequeue(); // queue is now [5]
 */
-class Queue {}
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  enqueue(value) {
+    return this.data.unshift(value);
+  }
+  dequeue() {
+    this.data.shift();
+    return "removed";
+  }
+  front() {
+    return this.data[0];
+  }
+  rear() {
+    return this.data.length - 1;
+  }
+  size() {
+    return this.data.length;
+  }
+}
+
+const queue = new Queue();
+
+queue.enqueue(5, 1);
+
+console.log(queue.dequeue());
 
 module.exports = Queue;
