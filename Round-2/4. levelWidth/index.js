5659f7784700cae65562766cb9ea6e9b9729e9e8
MousaHussen <Mousahusseen@gmail.com>
2022-07-18 12:26:49 +0300

    solve

--- Round-2/4. levelWidth/index.js
+++ Round-2/4. levelWidth/index.js
@@ -18,6 +18,44 @@ Answer: [1, 3, 2]
 
 const Node = require("./node");
 
-const levelWidth = (root) => {};
+const levelWidth = (root) => {
+  if (root == null) return 0;
+
+  let maxwidth = 0;
+  let arr = [];
+
+  arr.push(root);
+
+  while (arr.length > 0) {
+    let counter = 0;
+
+    maxwidth = Math.max(maxwidth, counter);
+
+    while (counter-- > 0) {
+      // Dequeue an node from queue
+      let temp = arr.shift();
+
+      // Enqueue left and right children
+      // of dequeued node
+      if (temp.left != null) {
+        arr.push(temp.left);
+      }
+      if (temp.right != null) {
+        arr.push(temp.right);
+      }
+    }
+  }
+  return maxwidth;
+};
+
+let root = new Node();
+
+root.add(1)
+root.add(2)
+root.add(3)
+
+
+
+console.log(levelWidth(2));
 
 module.exports = levelWidth;
